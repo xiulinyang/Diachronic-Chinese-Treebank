bb936e700bf69e897c24bb8c88737f54fb403e7e
XiulinYang <81375342+XiulinYang@users.noreply.github.com>
2021-05-15 23:04:38 +0100

    Codes for error analysis

--- error_analysis.py
+++ error_analysis.py
@@ -5,8 +5,10 @@ import nltk
 
 df_ddparser= pd.read_csv('sample_ddparser.conllx', delimiter ='\t', header= None)
 df_ddparser_gold = pd.read_csv('gold_ddparser.conllx', delimiter='\t', header = None)
+
+
+# print the id, deprel, and head of wrongly parsed sentences for analysis
 df_ddparser_gold.columns = ['ID', 'FORM','LEMMA','POS','FEATS','HEAD','DEPREL']
-# print the id, deprel, and head of wrongly parsed sentences
 
 '''Accuracy test of DDParser'''
 # read the head column in the sample corpus and gold corpus respectively
@@ -22,29 +24,29 @@ pos_ddparser = df_ddparser_gold[df_ddparser_gold.columns[3]]
 pos_ddparser2 = df_ddparser[df_ddparser.columns[3]]
 
 
-mishead_ddparser = []
-
 
 # find the misassigned head
+mishead_ddparser = []
 for i in range(len(head_ddparser2)):
     if head_ddparser[i] != head_ddparser2[i]:
         mishead_ddparser.append(rel_ddparser[i])
 
 # find the misassigned deprel
 misrel = []
-
 for i in range(len(rel_ddparser)):
     if rel_ddparser[i] != rel_ddparser2[i]:
         misrel.append(rel_ddparser[i])
 
 
-mispos_ddparser=[]
+
 # find the misassigned pos
+mispos_ddparser=[]
 for i in range(len(pos_ddparser2)):
     if pos_ddparser[i]!=pos_ddparser2[i]:
         mispos_ddparser.append(pos_ddparser[i])
 
 
+# print all wrongly labeled sentences
 wrong_sents = []
 for i in range(len(head_ddparser)):
     if head_ddparser[i] != head_ddparser2[i] or rel_ddparser[i] != rel_ddparser2[i] or pos_ddparser2[i]!= pos_ddparser[i]:
@@ -52,20 +54,26 @@ for i in range(len(head_ddparser)):
         wrong_sents.append(df_ddparser_gold.iloc[i])
 print(wrong_sents)
 
+
+# print sentences that are annoated with the wrong dependency relations AND pos
+# to see if they are related with each other.
+
 wrong_pos_rel =[]
-#
-# for i in range(len(head_ddparser2)):
-#     if rel_ddparser[i] != rel_ddparser2[i] and pos_ddparser2[i] != pos_ddparser[i]:
-#         wrong_pos_rel.append(df_ddparser_gold.index[i])
-#         wrong_pos_rel.append(df_ddparser_gold.iloc[i])
-#
-# print(wrong_pos_rel)
-# # get the frequency of wrongly parsed head and deprel
-# nltk.FreqDist(misrel).tabulate()
-# nltk.FreqDist(mishead_ddparser).tabulate()
-# nltk.FreqDist(mispos_ddparser).tabulate()
-# nltk.FreqDist(misrel).plot(15, cumulative=False, title='Corrected Relations in the Original Parsing Result')
-# nltk.FreqDist(mishead_ddparser).plot(15, cumulative=False,
-#                                      title='Corrected Heads in the Original Parsing Result')
-# nltk.FreqDist(mispos_ddparser).plot(18, cumulative=False,
-#                                      title='Corrected POSs in the Original Parsing Result')
+for i in range(len(head_ddparser2)):
+    if rel_ddparser[i] != rel_ddparser2[i] and pos_ddparser2[i] != pos_ddparser[i]:
+        wrong_pos_rel.append(df_ddparser_gold.index[i])
+        wrong_pos_rel.append(df_ddparser_gold.iloc[i])
+
+print(wrong_pos_rel)
+
+# get the frequency of wrongly parsed head and deprel
+nltk.FreqDist(misrel).tabulate()
+nltk.FreqDist(mishead_ddparser).tabulate()
+nltk.FreqDist(mispos_ddparser).tabulate()
+
+# visualize the errors
+nltk.FreqDist(misrel).plot(15, cumulative=False, title='Corrected Relations in the Original Parsing Result')
+nltk.FreqDist(mishead_ddparser).plot(15, cumulative=False,
+                                     title='Corrected Heads in the Original Parsing Result')
+nltk.FreqDist(mispos_ddparser).plot(18, cumulative=False,
+                                     title='Corrected POSs in the Original Parsing Result')
